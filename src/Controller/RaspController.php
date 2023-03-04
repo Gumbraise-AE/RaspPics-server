f44a8fcb5353fdfaf11cbe6a5fd344af669650c0
Gumbraise <vikiclashonyt@gmail.com>
2023-03-04 03:36:10 +0100

    Update RaspController.php

--- src/Controller/RaspController.php
+++ src/Controller/RaspController.php
@@ -49,8 +49,8 @@ class RaspController extends AbstractController
             $imagePath = $publicPath . $uploaderHelper->asset($raspPic, 'picFile');
             $frame->readImage($imagePath);
             $frame->resizeImage(800, 600, Imagick::FILTER_LANCZOS, 1);
-            $frame->setImageCompressionQuality(20);
-            $frame->setImageDelay(0);
+            $frame->setImageCompressionQuality(0);
+            $frame->setImageDelay(10);
             $imagick->addImage($frame);
         }
 
