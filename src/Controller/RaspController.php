733fb335ff27ca5a734bc443c6247fe002dd016c
Gumbraise <vikiclashonyt@gmail.com>
2023-03-04 02:57:04 +0100

    added some things

--- src/Controller/RaspController.php
+++ src/Controller/RaspController.php
@@ -3,9 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\RaspProject;
+use App\Repository\RaspPicRepository;
+use Imagick;
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+use Imagine\Image\ImageInterface;
+use Imagine\Image\Palette\RGB;
+use Imagine\Image\Point;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 #[Route('/rasp')]
 class RaspController extends AbstractController
@@ -18,5 +26,37 @@ class RaspController extends AbstractController
             'rasp' => $raspProject,
             'user_ip' => $_SERVER['REMOTE_ADDR']
         ]);
+    }
+
+    #[Route('/{id}/gif', name: 'app_single_rasp_gif')]
+    public function singleGif(
+        RaspProject       $raspProject,
+        RaspPicRepository $raspPicRepository,
+        UploaderHelper    $uploaderHelper,
+    ): Response
+    {
+        $raspPics = $raspPicRepository->findBy(['raspProject' => $raspProject], ["createdAt" => "DESC"], 48);
+
+        // Créer un objet Imagick pour créer le GIF
+        $imagick = new Imagick();
+
+        $publicPath = $this->getParameter('kernel.project_dir') . '/public';
+
+        // Ajouter chaque image à l'objet Imagick
+        foreach ($raspPics as $raspPic) {
+            $imagePath = $publicPath . $uploaderHelper->asset($raspPic, 'picFile');
+            $imagick->readImage($imagePath);
+            $imagick->setImageDelay(25);
+        }
+
+        // Écrire le GIF dans une variable
+        $gifData = $imagick->getImagesBlob();
+
+        // Renvoyer le GIF en tant que réponse HTTP
+        return new Response($gifData, 200, [
+            'Content-Type' => 'image/gif',
+            'Content-Disposition' => 'attachment; filename="raspPics.gif"',
+        ]);
+
     }
 }
